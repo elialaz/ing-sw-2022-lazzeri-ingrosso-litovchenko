c3aa0f8eff99a37b22e82fbcec5f3f027772cc8d
elia_laz <elia.lazzeri@mail.polimi.it>
2022-04-21 21:33:31 +0200

    Java Class model modification

--- src/main/java/it/polimi/ingsw/Model/Bag.java
+++ src/main/java/it/polimi/ingsw/Model/Bag.java
@@ -1,4 +1,23 @@
 package it.polimi.ingsw.Model;
 
+import java.util.ArrayList;
+
 public class Bag {
+    private int extracted;
+    public Bag(){
+        this.extracted = 0;
+    }
+
+    public ArrayList<Student> getStudent(int num){
+        ArrayList<Student> generated = new ArrayList<Student>();
+        for (int i = 0; i < num; i++){
+            generated.add(new Student(Color.randomLetter()));
+        }
+        this.extracted += num;
+        return generated;
+    }
+
+    public int getExtracted() {
+        return extracted;
+    }
 }
--- src/main/java/it/polimi/ingsw/Model/Player.java
+++ src/main/java/it/polimi/ingsw/Model/Player.java
@@ -1,4 +1,21 @@
 package it.polimi.ingsw.Model;
 
+import java.util.ArrayList;
+
 public class Player {
+
+    private String nikname;
+    private ArrayList<Card> assistantCards;
+    private SchoolBoard board;
+    private TowerColor tower;
+    private int NumCoin;
+    private Boolean playInTeam;
+
+    public Player(String playerName){
+
+    }
+
+    public void addNumCoin(int num) {
+        this.NumCoin += num;
+    }
 }
