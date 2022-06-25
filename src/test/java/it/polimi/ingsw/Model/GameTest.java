e12e6e820f4d9758e589e4fc193017f211b7505e
litovn <niki.litovchenko@gmail.com>
2022-06-25 16:07:54 +0200

    Test suf coverage

--- src/test/java/it/polimi/ingsw/Model/GameTest.java
+++ src/test/java/it/polimi/ingsw/Model/GameTest.java
@@ -11,21 +11,6 @@ import java.util.ArrayList;
 class GameTest {
    private final Game game = new Game(2, "elvis", 56, true);
    private CloudTile cloud = new CloudTile(4);
-   private SchoolBoard school = new SchoolBoard(TowerColor.WHITE, 6, new int[]{2,3,1,3,0});
-   private SchoolBoard school2 = new SchoolBoard(TowerColor.BLACK, 6, new int[]{3,2,1,1,2});
-   private MotherNature mom = new MotherNature();
-    /*
-    @Test
-    void addPlayer() throws ToMuchPlayerExcetpion {
-        //Assertion to do, but Exception class not defined
-        gameTest.addPlayer("Xx");
-        assertEquals(2, gameTest.getPlayerNum());
-    }*/
-
-    @Test
-    void addPlayer() throws ToMuchPlayerExcetpion{
-
-    }
 
     @Test
     void setPlusTwoEffectPlayer() {
@@ -74,8 +59,6 @@ class GameTest {
 
     @Test
     void TakeCloudTile() {
-        cloud = new CloudTile(4);
-        cloud.setStudents(new Bag(5));
         ArrayList<SchoolBoard> schoolEntrance = game.getSchoolboards();
         game.takeCloudTile(0,0);
         int[] entrance = schoolEntrance.get(0).getEntranceStudents();
@@ -83,7 +66,7 @@ class GameTest {
         for (int i=0; i<entrance.length; i++){
             totEntrance += entrance[i];
         }
-        assertEquals(5, totEntrance);
+        assertEquals(10, totEntrance);
     }
 
     @Test
@@ -92,7 +75,7 @@ class GameTest {
         assertEquals(4, game.getLastCardMovementAllowed(0));
     }
 
-    /*
+
     @Test
     void MoveStudentsToSchoolBoard() {
         SchoolBoard schoolBoard = game.getSchoolboards().get(0);
@@ -105,31 +88,14 @@ class GameTest {
 
     @Test
     void moveStudentsToIsland() {
+        int[] init = game.getSchoolboards().get(0).getEntranceStudents();
+        int[] remove = new int[]{1,0,1,1,0};
+        game.moveStudentsToIsland(remove, 0, game.getIslandTile().get(1));
+        for (int i=0; i<5; i++){
+            init[i] -= remove[i];
+        }
+        assertArrayEquals(init, game.getSchoolboards().get(0).getEntranceStudents());
     }
-
-
-    @Test
-    void checkProfessorInfluence() {
-        ArrayList<SchoolBoard> schoolBoards = game.getSchoolboards();
-        schoolBoards.get(0).moveCorridor(new int[]{0, 2, 1, 0, 0});
-        Player player1 = new Player("nik",1,0);
-        schoolBoards.get(1).setPlayer(player1);
-        schoolBoards.get(1).moveCorridor(new int[]{0, 2, 0, 1, 0});
-        game.checkProfessorInfluence(schoolBoards.get(1));
-        assertEquals(false, schoolBoards.get(0).isProfessor(0));
-        assertEquals(true, schoolBoards.get(0).isProfessor(1));
-        assertEquals(true, schoolBoards.get(0).isProfessor(2));
-        assertEquals(false, schoolBoards.get(0).isProfessor(3));
-        assertEquals(false, schoolBoards.get(0).isProfessor(4));
-    }*/
-
-    //TODO quanto checkControl e Union testate
-    /*@Test
-    void moveMotherNature() {
-        int mom_pos = mom.getPosition();
-        game.moveMotherNature(3);
-        assertEquals(mom_pos+3, mom.getPosition());
-    }*/
 
     @Test
     void checkControl() {
@@ -198,14 +164,5 @@ class GameTest {
     @Test
     void islandNUm() {
         assertEquals(12, game.islandNUm());
-    }
-
-    @Test
-    void checkTowerNum() {
-        ArrayList<SchoolBoard> schoolBoards = game.getSchoolboards();
-        schoolBoards.get(0).removeTower(1);
-        schoolBoards.get(1).removeTower(2);
-        schoolBoards.get(2).removeTower(2);
-        assertEquals("elvis", game.checkTowerNum());
     }
 }
